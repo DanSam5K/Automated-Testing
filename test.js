b343e15910f8b8016c40a1fcab5e6b8ecfa65026
Daniel Samuel <dansam5000@gmail.com>
2021-08-24 23:12:05 +0100

    Unit test for reverseString function

--- test.js
+++ test.js
@@ -1,13 +1,27 @@
 //import { it, test } from '@jest/globals';
 const { string } = require('yargs');
-const stringLength  = require('./stringlength.js');
+const stringLength  = require('./stringlength');
+const stringReverse  = require('./stringreverse');
+const Calculator = require('./calculator');
 
 
 it ('Takes string and return it characters count', () => {
-    let str = "Man";
+    let str = "Man of "; 
+
     const result = stringLength(str);
-    expect(result).toBe(3)
+
+    expect(result).toBe(7)
     expect(result).not.toBe(0)
     expect(result).toBeLessThan(11);
+})
+
+
+it ('reverse string', () => {
+    let str = "Man";
+
+    const result = stringReverse(str);
+
+    expect(result).toBe('nam')
+    expect(result).not.toBe("man")
 })
    
